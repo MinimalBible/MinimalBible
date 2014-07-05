60185c934a577692a70dda3dd161c4d0b7bc83bd
DjBushido <bspeice.nc@gmail.com>
2014-07-05 15:59:34 -0400

    Test cases pass now with the navbar

--- app/src/androidTest/java/org/bspeice/minimalbible/DownloadActivityTest.java
+++ app/src/androidTest/java/org/bspeice/minimalbible/DownloadActivityTest.java
@@ -1,26 +1,24 @@
 package org.bspeice.minimalbible;
 
 import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
 import android.util.Log;
 
 /**
  * Created by Bradlee Speice on 7/5/2014.
  */
-public class DownloadActivityTest extends ActivityUnitTestCase<DownloadActivity> {
+/* Please note that it is necessary to extend the ActivityInstrumentationTestCase2 if you plan
+    on using an activity that sets up any underlying fragments (includes navigation drawers).
+    The ActivityUnitTestCase doesn't set up enough of the Activity lifecycle.
+ */
+public class DownloadActivityTest extends ActivityInstrumentationTestCase2<DownloadActivity> {
 
     public DownloadActivityTest() {
         super(DownloadActivity.class);
     }
 
     public void testAndroidTestInjection() {
-        MinimalBible m = new MinimalBible();
-        m.buildObjGraph();
-        setApplication(m);
-
-        startActivity(new Intent(getInstrumentation().getTargetContext(),
-                DownloadActivity.class), null, null);
-
         DownloadActivity a = getActivity();
         assertNotNull(a);
 
--- app/src/main/java/org/bspeice/minimalbible/DownloadActivity.java
+++ app/src/main/java/org/bspeice/minimalbible/DownloadActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -45,12 +46,11 @@ public class DownloadActivity extends ActionBarActivity
         mTitle = getTitle();
         Log.w("DownloadActivity", "Title: " + mTitle.toString());
 
-        /*
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
-                */
+
     }
 
     @Override
