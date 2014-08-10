6271cc9626073a960fb81f58c76305eafc00d558
Bradlee Speice <bspeice.nc@gmail.com>
2014-08-09 20:52:02 -0400

    Figured out why I was getting some initial errors.

--- app/src/main/java/org/bspeice/minimalbible/activity/viewer/BookManager.java
+++ app/src/main/java/org/bspeice/minimalbible/activity/viewer/BookManager.java
@@ -3,12 +3,16 @@ package org.bspeice.minimalbible.activity.viewer;
 import org.crosswire.jsword.book.Book;
 import org.crosswire.jsword.book.Books;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import rx.Observable;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -20,10 +24,22 @@ public class BookManager {
     private Observable<Book> installedBooks;
     private Boolean refreshComplete;
 
+    // Some of these books seem to think they're installed...
+    private List<String> excludeBooks = new ArrayList<String>() {{
+        add("ERen_no");
+        add("ot1nt2");
+    }};
+
     @Inject
     BookManager() {
         // TODO:  Any way this can be sped up goes straight to the initialization time.
         installedBooks = Observable.from(Books.installed().getBooks())
+                .filter(new Func1<Book, Boolean>() {
+                    @Override
+                    public Boolean call(Book book) {
+                        return !excludeBooks.contains(book.getInitials());
+                    }
+                })
             .cache();
         installedBooks.subscribeOn(Schedulers.io())
                 .subscribe(new Action1<Book>() {
