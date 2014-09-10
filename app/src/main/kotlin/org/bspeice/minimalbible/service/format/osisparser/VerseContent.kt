3d8ff65af44df88ce5164747790642f23e8c86d8
Bradlee Speice <bspeice.nc@gmail.com>
2014-09-09 23:31:32 -0400

    Bringing back Kotlin I can do manual DI for testing, other Android classes can handle interfacing with Kotlin. From my testing, interop is quite smooth.

---
+++ app/src/main/kotlin/org/bspeice/minimalbible/service/format/osisparser/VerseContent.kt
@@ -0,0 +1,29 @@
+/**
+ * Created by bspeice on 9/9/14.
+ */
+package org.bspeice.minimalbible.service.format.osisparser
+
+import com.google.gson.Gson
+import org.crosswire.jsword.passage.Verse
+import java.util.ArrayList
+
+class VerseContent(v: Verse?) {
+    var id = v?.getOrdinal() ?: 0
+    var bookName = v?.getName() ?: ""
+    var chapter = v?.getChapter() ?: 0
+    var verseNum = v?.getVerse() ?: 0
+    var content = ""
+    var chapterTitle = ""
+    var paraTitle = ""
+    var references: MutableList<VerseReference> = ArrayList()
+
+    public fun toJson(): String {
+        // Lazy load Gson - not likely that we'll call this method multiple times, so
+        // don't have to worry about a penalty there.
+        return Gson().toJson(this) as String
+    }
+
+    public fun appendContent(content: String) {
+        this.content += content
+    }
+}
---
+++ app/src/main/kotlin/org/bspeice/minimalbible/service/format/osisparser/VerseReference.kt
@@ -0,0 +1,12 @@
+package org.bspeice.minimalbible.service.format.osisparser
+
+import org.crosswire.jsword.passage.Verse
+
+/**
+ * Created by bspeice on 9/9/14.
+ */
+
+class VerseReference(verse: Verse, index: Int) {
+    val verse: Verse = verse
+    val index: Int = index
+}
