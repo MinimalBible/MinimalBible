35b515add764de747d3f9820124f05a42094e7dc
Bradlee Speice <bspeice.nc@gmail.com>
2014-11-11 23:26:01 -0500

    Small documentation update on Injector

--- app/src/main/java/org/bspeice/minimalbible/Injector.java
+++ app/src/main/java/org/bspeice/minimalbible/Injector.java
@@ -1,7 +1,8 @@
 package org.bspeice.minimalbible;
 
 /**
- * Created by bspeice on 7/2/14.
+ * Define a simple interface for classes to receive an object to provide their dependencies.
+ * This way, the dependencies can be provided by an activity, or a test module, etc.
  */
 public interface Injector {
     public void inject(Object o);
