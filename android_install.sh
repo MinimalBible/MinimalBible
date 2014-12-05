20d54645f8972be253997422eb3df4f0903091a5
Bradlee Speice <bspeice.nc@gmail.com>
2014-12-05 01:09:39 -0500

    android_install takes care of everything

--- android_install.sh
+++ android_install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash - 
 
+COMPONENTS="
+tools
+platform-tools
+build-tools-21.1.1
+android-21
+extra-android-m2repository
+extra-android-support
+extra-google-m2repository"
+
 wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
 tar xf android-sdk_r23.0.2-linux.tgz
 cd android-sdk-linux/
 export ANDROID_HOME=`pwd`
+ls android-sdk-linux/
 
-COMPONENT_ARRAY=$(echo $COMPONENTS | tr "," "\n")
-for COMP in $COMPONENT_ARRAY
+for COMP in ${COMPONENTS}
 do
-	echo "yes" | tools/android update sdk -u -a -t $COMP
+	echo "yes" | tools/android update sdk -u -a -t ${COMP}
 done
 
 cd -
