49e508759502f7f7401d315ebd91e7fa289415b1
Bradlee Speice <bspeice.nc@gmail.com>
2014-12-05 01:16:14 -0500

    Have to use Travis to set ANDROID_HOME

--- android_install.sh
+++ android_install.sh
@@ -11,14 +11,10 @@ extra-google-m2repository"
 
 wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
 tar -zxvf android-sdk_r23.0.2-linux.tgz
-cd android-sdk-linux/
-export ANDROID_HOME=`pwd`
-ls
-ls *
 
 for COMP in ${COMPONENTS}
 do
-	echo "yes" | tools/android update sdk -u -a -t ${COMP}
+	echo "yes" | android-sdk-linux/tools/android update sdk -u -a -t ${COMP}
 done
 
 cd -
