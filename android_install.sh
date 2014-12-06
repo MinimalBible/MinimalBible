21242728ce47a6e3753f599ea09326345201338b
Bradlee Speice <bspeice.nc@gmail.com>
2014-12-05 22:56:03 -0500

    xPm, not xDm. My bad.

--- android_install.sh
+++ android_install.sh
@@ -10,7 +10,7 @@ extra-android-support
 extra-google-m2repository"
 
 sudo apt-get update
-sudo apt-get install --force-yes libgd2-xdm ia32-libs ia32-libs-multiarch
+sudo apt-get install --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
 wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
 tar -zxf android-sdk_r23.0.2-linux.tgz
 
