460a0403a87a3fe5839c20f2a8c35e7e8533db5f
Bradlee Speice <bspeice.nc@gmail.com>
2014-12-05 22:54:05 -0500

    Clean up build output, reinstall libgd2-xdm

--- android_install.sh
+++ android_install.sh
@@ -10,11 +10,11 @@ extra-android-support
 extra-google-m2repository"
 
 sudo apt-get update
-sudo apt-get install -qq --force-yes ia32-libs ia32-libs-multiarch
+sudo apt-get install --force-yes libgd2-xdm ia32-libs ia32-libs-multiarch
 wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
-tar -zxvf android-sdk_r23.0.2-linux.tgz
+tar -zxf android-sdk_r23.0.2-linux.tgz
 
 for COMP in ${COMPONENTS}
 do
-	echo "yes" | android-sdk-linux/tools/android update sdk -u -a -t ${COMP}
+	echo "yes" | android-sdk-linux/tools/android update sdk -u -a -t ${COMP} > /dev/null
 done
