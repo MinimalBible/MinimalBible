be8e7ff56015f9e55751bc095d8dd3649d77bde2
Bradlee Speice <bspeice.nc@gmail.com>
2014-12-05 23:07:22 -0500

    Last build tweak for a while, promise.

--- android_install.sh
+++ android_install.sh
@@ -9,13 +9,17 @@ extra-android-m2repository
 extra-android-support
 extra-google-m2repository"
 
+echo "Updating packages..."
 sudo apt-get update -qq
+
+echo "Installing Android dependencies..."
 sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
+
 wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
 tar -zxf android-sdk_r23.0.2-linux.tgz
 
 for COMP in ${COMPONENTS}
 do
-	echo "Installing: $COMP"
+	echo "Installing Android component: $COMP"
 	echo "yes" | android-sdk-linux/tools/android update sdk -u -a -t ${COMP} > /dev/null
 done
